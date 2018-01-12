6362fa195f340d678e0ffbdaf9251655646fc8c6
Fabian_Simon <fasibio@gmail.com>
2018-01-12 22:30:13 +0100

    Fine complete way from generate VHOST file until reboot apache

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -46,5 +47,44 @@ func main() {
 	}
 	fmt.Println(newFilename)
 	w.Flush()
+	moveFileToApacheConfig(newFilename)
+	doApacheConfigTest(newFilename)
+	restartApache()
+}
 
+func moveFileToApacheConfig(configFile string) {
+	fmt.Println("moveFileToApache sites-enabled")
+	err := os.Rename(configFile, "/etc/apache2/sites-enabled/"+configFile)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func doApacheConfigTest(configFile string) {
+	fmt.Println("test apache config")
+	args := []string{"configtest"}
+	cmd := exec.Command("/usr/sbin/apachectl", args...)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error by Configtest remove Config file from sites-enable")
+		os.Remove("/etc/apache2/sites-enabled/" + configFile)
+		panic(err)
+	}
+	// output, err2 := cmd.Output()
+	// fmt.Println("hier", output)
+	// if err2 != nil {
+	// 	panic(err2)
+	// }
+}
+
+func restartApache() {
+	fmt.Println("Restart Apache2")
+	args := []string{"restart", "apache2"}
+	cmd := exec.Command("/bin/systemctl", args...)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error by restart Apache2 Server")
+		panic(err)
+	}
 }
